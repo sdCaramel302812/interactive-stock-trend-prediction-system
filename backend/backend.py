4b55295d478542750a73ce8f45a73a2a4a5e186b
sandyhhsiao <sandyhsiao8545@gmail.com>
2022-12-05 19:39:59 -0500

    Merge with model

--- backend/backend.py
+++ backend/backend.py
@@ -1,11 +1,18 @@
 from flask import Flask, request 
-from  sklearn.linear_model import LinearRegression 
-import numpy as np
+from sklearn.linear_model import LinearRegression  
+from sklearn.preprocessing import MinMaxScaler 
+from tensorflow import keras 
+import math
+import numpy as np 
+import pandas as pd  
+import tensorflow as tf  
 
 
 app = Flask(__name__)   
 
-models = {
+models = { 
+    "df" : None, 
+
     "train_x" : None, 
     "trainingPrice" : None, 
     "trainingDate" : None,  
@@ -15,15 +22,58 @@ models = {
     "predictedPrice" : None, 
     "predictedDate" : None,  
 
+    "label_scaler" : None, 
+
     "coef_value" : None, 
     "coef_name" : None, 
 } 
 
+def getScaledData():
+    df_scaler = MinMaxScaler(feature_range=(0,1))
+    scaled_data = df_scaler.fit_transform(models["df"])
+    
+    label = models["df"]["close"].values
+    label_scaler = MinMaxScaler(feature_range=(0,1))
+    scaled_label = label_scaler.fit_transform(label.reshape(-1,1))
+    
+    return scaled_data, scaled_label, label_scaler
+
+def getPredictionFromLSTM(training_data_len, scaled_data, scaled_label, model):
+    # Train data
+    train_data = scaled_data[0: training_data_len, :]
+    x_train = []
+    y_train = []
+    for i in range(30, len(train_data)):
+        x_train.append(train_data[i-30:i, ])
+        y_train.append(train_data[i, ])
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2]))
+    
+    # Test Data
+    test_data = scaled_data[training_data_len-30: , : ]
+    x_test = []
+    y_test = []
+    for i in range(30, len(test_data)):
+        x_test.append(test_data[i-30: i, ])
+        y_test.append(test_data[i, ])
+    x_test, y_test = np.array(x_test), np.array(y_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], x_test.shape[2]))
+    
+    predicted_train = model.predict(x_train)
+    train_label = scaled_label[30:x_train.shape[0]+30]
+    predicted_test = model.predict(x_test)
+    test_label = scaled_label[x_train.shape[0]+30:]
+    
+    return predicted_train, train_label.reshape(-1), predicted_test, test_label.reshape(-1)
+
+
 def fit_linear_regression(): 
     linearReg = LinearRegression(fit_intercept=False)
     fittedReg = linearReg.fit(models["train_x"], models["trainingPrice"])
-    models["coef_value"] = fittedReg.coef_ 
-    models["predictedPrice"] = fittedReg.predict(models["test_x"]) 
+    models["coef_value"] = fittedReg.coef_   
+
+    predictions = fittedReg.predict(models["test_x"]) 
+    models["predictedPrice"] = models["label_scaler"].inverse_transform(predictions.reshape(-1,1))
 
 @app.route('/GetStockData', methods=["GET"])
 def predict_result(): 
@@ -46,22 +96,29 @@ def adjust_coefficient():
     predicted = np.zeros(models["test_x"].shape[0]) 
     for i in range(models["test_x"].shape[0]): 
         predicted[i] = np.dot(models["test_x"][i], models["coef_value"]) 
-    models["predictedPrice"] = predicted
+    
+    models["predictedPrice"] = models["label_scaler"].inverse_transform(predicted.reshape(-1,1))
 
     return {"coefficient": models["coef_value"], "predictedPrice": models["predictedPrice"]} 
 
 @app.route('/')  
-def init():  
-    models["train_x"] = np.random.rand(10, 23) 
-    models["trainingPrice"] = np.arange(10, 20)
-    models["trainingDate"] = None  
+def init():   
+    models["df"] = pd.read_pickle('../data/sp500.pkl')   
+    model = keras.models.load_model('../lstm/models/best_model.h5') 
+    training_data_len = math.ceil(models["df"]["close"].values.shape[0] * 0.8)  
 
-    models["test_x"] = np.random.rand(3, 23) 
-    models["realPrice"] = np.arange(20, 30)
-    models["predictedDate"] = None 
-    models["predictedPrice"] = None  
+    scaled_data, scaled_label, models["label_scaler"] = getScaledData() 
+    predicted_train, train_label, predicted_test, test_label = getPredictionFromLSTM(training_data_len, scaled_data, scaled_label, model)
+      
+    models["trainingDate"] = models["df"].index[30:training_data_len]  
+    models["trainingPrice"] = train_label
+    models["train_x"] = predicted_train 
 
-    models["coef_name"] = None
+    models["predictedDate"] = models["df"].index[training_data_len:] 
+    models["realPrice"] = test_label
+    models["test_x"] = predicted_test
+
+    models["coef_name"] = models["df"].columns
 
     fit_linear_regression() 
 
